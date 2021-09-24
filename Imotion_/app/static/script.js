d8ddf0b9d8321e60f74af3c650eac0da027dc756
Ahana Datta <ahana.datta.ravenclaw@gmail.com>
2021-09-24 17:46:52 +0100

    anger, happiness done, drone models added

--- Imotion_/app/static/script.js
+++ Imotion_/app/static/script.js
@@ -6,22 +6,40 @@ const renderer = new THREE.WebGLRenderer({antialias: true});
 renderer.setSize( window.innerWidth, window.innerHeight );
 renderer.setClearColor("#90e0ef");
 document.body.appendChild( renderer.domElement );
-const geometry = new THREE.BoxGeometry(0.7,0.7,0.4);
-const material = new THREE.MeshLambertMaterial( { color: 0x00df00 } );
+var geometry = new THREE.BoxGeometry(0.7,0.7,0.4);
+var material = new THREE.MeshLambertMaterial( { color: 0xb90e0a } );
 var cubes = []
-const cube = new THREE.Mesh( geometry, material );
-cube.position.x = 6;
+const cube_ = new THREE.Mesh( geometry, material );
+var cube = new THREE.Group()
+var model_loader = new THREE.GLTFLoader();
+model_loader.load(
+    './static/drone.glb',
+    function ( gltf ) {
+    
+		console.log(gltf)
+        cube = gltf.scene.getObjectByName("Syma_X8C001")
+        cube.scale.set(0.07,0.07,0.07)
+        THREE.BufferGeometry.prototype.copy.call(geometry, cube.geometry);
+    }, undefined, function ( error ) {
+        console.error( error );
+        console.log("wow")
+    } 
+);
 
+const num_bots = 12;
 var light = new THREE.PointLight(0xFFFF00);
 light.position.set(5, 0, 35);
 scene.add(light);
 
 function addbots(){
-	var l = 12
+	var l = num_bots
 	var step = 2*Math.PI / l;
+    //var acube = new THREE.Group()
 	for(var i =0;i<l;i++){
 		var acube = new THREE.Mesh( geometry, material );
-		acube.position.x = 5
+        acube.scale.set(0.07,0.07,0.07)
+        acube.rotation.x +=1;
+		//acube = cube.clone()
 		acube.position.x = r*Math.cos(i*step) ;
 		acube.position.y = r*Math.sin(i*step) ;
 		
@@ -42,17 +60,26 @@ var sad_r = 2.1;
 var anger_r=2;
 var anger_sr = 1;
 var sinx = 0;
+var arr=[];
+var ar = [];
+for(var arr_i=0;arr_i<num_bots; arr_i++){
+	for(var arr_j=0;arr_j<2; arr_j++){
+		ar.push(Math.random()*0.3 + 0.01);
+	}
+	arr.push(ar);
+}
+
 function animate() {
 	requestAnimationFrame( animate );
 	emotion = document.getElementById('emotion').value;
 	if(emotion==="sadness"){
 		t+=0.001
-		var step = 2*Math.PI / 12;
+		var step = 2*Math.PI / num_bots;
 		var i = 0;
 		cubes.forEach(cube=>{
 			cube.position.x = sad_r*Math.cos(t + i*step) ;
 			cube.position.y = sad_r*Math.sin(t + i*step) ;
-			cube.rotation.z = Math.atan2( cube.position.y, cube.position.x ) ;
+			cube.rotation.y = Math.atan2( cube.position.y, cube.position.x ) ;
 			i += 1;
 		});
     }
@@ -79,38 +106,25 @@ function animate() {
 		cubes[i].position.x = 0.4 ;
 		cubes[i].position.y = 0 ;
 		t+=0.05
-		sinx+=0.5
-		var step = 2*Math.PI / 12;
+		var step = 2*Math.PI / num_bots;
 		var i = 0;
-		var arr=[];
-		var ar = [];
-		for(var arr_i=0;arr_i<12; arr_i++){
-			for(var arr_j=0;arr_j<2; arr_j++){
-				ar.push(Math.random()*0.6);
-			}
-			arr.push(ar);
-		}
-		cubes.forEach(cube=>{
-			// sinx=sinx%(2*Math.PI);
-			// cube.position.x = hap_r*Math.cos(t + i*step) + sinx;
-			// cube.position.y = hap_r*Math.sin(t + i*step) + Math.sin(sinx);
-			// cube.rotation.z = Math.atan2( cube.position.y, cube.position.x ) ;
+		
+		for(i=0;i<num_bots;i++){
 			
-			cube.position.x += i;
-			cube.position.y += 0.5*Math.cos(t)*Math.sin(0.5);
-			cube.rotation.z = Math.atan2( cube.position.y, cube.position.x ) ;
-			i += 1;
-		});
+			cubes[i].position.x += arr[i][0]*Math.sin(t + i*Math.PI*0.5);
+			cubes[i].position.y += arr[i][1]*Math.sin(t+i*Math.PI*0.5)
+			cubes[i].rotation.y = Math.atan2( cubes[i].position.y, cubes[i].position.x ) ;
+		};
 		
     }
     else if(emotion==="surprise"){
 		t+=0.005
-		var step = 2*Math.PI / 12;
+		var step = 2*Math.PI / num_bots;
 		var i = 0;
 		cubes.forEach(cube=>{
 			cube.position.x = sur_r*Math.cos(t + i*step) ;
 			cube.position.y = sur_r*Math.sin(t + i*step) ;
-			cube.rotation.z = Math.atan2( cube.position.y, cube.position.x ) ;
+			cube.rotation.y = Math.atan2( cube.position.y, cube.position.x ) ;
 			i += 1;
 		});
 		
@@ -123,25 +137,17 @@ function animate() {
 		} 
     }
 	else if(emotion==="happiness"){
-		// t+=0.5
 		sinx+=0.005
 		sinx=sinx%(2*Math.PI);
-		var step = 2*Math.PI / 12;
+		var step = 2*Math.PI / num_bots;
 		var i = 0;
 		cubes.forEach(cube=>{
 			cube.position.x = (3+0.5*Math.cos(10*sinx + i*(Math.PI)))*Math.cos(sinx+ i*step);
 			cube.position.y = (3+0.5*Math.cos(10*sinx + i*(Math.PI)))*Math.sin(sinx+ i*step);
-			cube.rotation.z = Math.atan2( cube.position.y, cube.position.x ) ;
+			cube.rotation.y = Math.atan2( cube.position.y, cube.position.x ) ;
 			i += 1;
 		});
-		
-		sur_r += incr;
-		if(sur_r>4){
-			incr = -incr;
-		}
-		else if(sur_r<2){
-			incr = -incr;
-		} 
+		 
     }
 	
 	renderer.render( scene, camera );
